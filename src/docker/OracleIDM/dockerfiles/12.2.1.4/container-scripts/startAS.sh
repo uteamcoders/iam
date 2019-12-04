b76f9ee8612a269254f5e45f901da69f9d12f889
ivasi <wtf@discountify.me>
2019-12-04 19:42:49 +0100

    create domain scripts done!

--- src/docker/OracleIDM/dockerfiles/12.2.1.4/container-scripts/startAS.sh
+++ src/docker/OracleIDM/dockerfiles/12.2.1.4/container-scripts/startAS.sh
@@ -33,7 +33,7 @@ mkfifo "${statusfile}" || exit 1
 {
   # run tail in the background so that the shell can kill tail when notified 
   # that grep has exited
-  tail -F ${DOMAIN_HOME}/servers/AdminServer/AdminServer.log &
+  tail -F ${DOMAIN_HOME}/servers/AdminServer/logs/AdminServer.log &
 
   # remember tail's PID
   tailpid=$!
