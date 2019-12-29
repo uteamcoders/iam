6801526e6fd361bbf9fdde08632c6fdd794bdc9d
ivasi <wtf@discountify.me>
2019-12-29 21:18:16 +0100

    test Liferay DB  - json for REST API and transform provisioning

---
+++ OracleIdentityGovernance/src/test/liferay/prove_transf.groovy
@@ -0,0 +1,5 @@
+SCREENNAME = context.beneficiary.getAttribute("User Login");
+FIRSTNAME = context.beneficiary.getAttribute("First Name");
+LASTNAME = context.beneficiary.getAttribute("Last Name");
+EMAILADDRESS = context.beneficiary.getAttribute("Email");
+PASSWORD = context.beneficiaryPassword;
