bbc1b2bb215690740cbb48a6d99f38135ff88bc7
ivasi <wtf@discountify.me>
2019-12-30 15:52:48 +0100

    fix session timeouts script

---
+++ OracleIdentityGovernance/src/docker/OracleIDM/dockerfiles/12.2.1.4/container-scripts/fix_session_timeout.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+set -x
+echo "Usage: fix_session_timeout.sh OIM_API_URL OIM_API_USER PASSWORD"
+cp -R /u01/iam-self-service.Plan.xml /u01/oracle/user_projects/domains/idm_domain
+cp -R /u01/iam-sysadmin.Plan.xml /u01/oracle/user_projects/domains/idm_domain
+
+curl -v --user $2:$3 \
+-H X-Requested-By:utcoders \
+-H Accept:application/json \
+-H Content-Type:application/json \
+-d "{
+ name: 'oracle.iam.console.identity.self-service.ear',
+ applicationPath : '/u01/oracle/idm/server/apps/oracle.iam.console.identity.self-service.ear',
+ targets: ['oim_server1'],
+ plan: '/u01/oracle/user_projects/domains/idm_domain/iam-self-service.Plan.xml',
+ deploymentOptions: {}
+}" \
+-X POST $1/management/weblogic/latest/domainRuntime/deploymentManager/deploy
+
+curl -v --user $2:$3 \
+-H X-Requested-By:utcoders \
+-H Accept:application/json \
+-H Content-Type:application/json \
+-d "{
+ name: 'oracle.iam.console.identity.sysadmin.ear',
+ applicationPath : '/u01/oracle/idm/server/apps/oracle.iam.console.identity.sysadmin.ear',
+ targets: ['oim_server1'],
+ plan: '/u01/oracle/user_projects/domains/idm_domain/iam-sysadmin.Plan.xml',
+ deploymentOptions: {}
+}" \
+-X POST $1/management/weblogic/latest/domainRuntime/deploymentManager/deploy
